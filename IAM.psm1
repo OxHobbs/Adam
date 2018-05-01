a6804a71bc0aae7a0cf789581060079467ffee8d
Austin Hobbs <austin.hobbs@microsoft.com>
2018-05-01 12:02:28 -0500

    added helpers path back with keep file

--- IAM.psm1
+++ IAM.psm1
@@ -4,8 +4,7 @@
 #Requires -Version 4
 $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 
-# "$moduleRoot\helpers\*.ps1", 
-"$moduleRoot\Functions\*.ps1" | Resolve-Path | Where-Object { -not ($_.ProviderPath.ToLower().Contains(".tests.")) } |
+"$moduleRoot\helpers\*.ps1", "$moduleRoot\Functions\*.ps1" | Resolve-Path | Where-Object { -not ($_.ProviderPath.ToLower().Contains(".tests.")) } |
 	ForEach-Object { . $_.ProviderPath }
 
 
