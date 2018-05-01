a5bd643389124b08cea925ea33c2aa02738df898
Austin Hobbs <austin.hobbs@microsoft.com>
2018-05-01 09:16:06 -0500

    keep file

--- IAM.psm1
+++ IAM.psm1
@@ -4,7 +4,8 @@
 #Requires -Version 4
 $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 
-"$moduleRoot\helpers\*.ps1", "$moduleRoot\Functions\*.ps1" | Resolve-Path | Where-Object { -not ($_.ProviderPath.ToLower().Contains(".tests.")) } |
+# "$moduleRoot\helpers\*.ps1", 
+"$moduleRoot\Functions\*.ps1" | Resolve-Path | Where-Object { -not ($_.ProviderPath.ToLower().Contains(".tests.")) } |
 	ForEach-Object { . $_.ProviderPath }
 
 
