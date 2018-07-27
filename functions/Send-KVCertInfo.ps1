67e098c85ff09d7bc2c8460bca8c6e5a8b040180
Austin Hobbs <austin.hobbs@microsoft.com>
2018-07-27 10:09:59 -0500

    updated parameter defaults for send-kvcertinfo

--- functions/Send-KVCertInfo.ps1
+++ functions/Send-KVCertInfo.ps1
@@ -1,3 +1,43 @@
+<#
+.SYNOPSIS
+Send data about certificates stored in Azure Key Vault to a custom Log Analytics table.
+
+.DESCRIPTION
+This cmdlet will send data about Certificates stored in Azure Key Vault to a custom Log Analytics table.  Tags that are 
+provided in the 'ValidTags' parameter will also be sent to Log Analytics.  The Certificates that are collected may be
+pulled from a provided Threshold.  By default, all certificate data is collected and forwarded.
+
+.PARAMETER VaultResourceGroup
+Provide the resource group in which the Azure Key Vault exists.
+
+.PARAMETER VaultName
+Provide the name of the Azure Key Vault
+
+.PARAMETER VaultSubscriptionName
+Provide the name of the subscription in which the Azure Key Vault exists.  This can be ommited if the subscription is already in the
+correct context.
+
+.PARAMETER LogAnalyticsWorkspaceName
+Provide the name of the Log Analytics workspace to which the data will be forwarded.
+
+.PARAMETER LogAnalyticsSubscriptionName
+Specify the name of the subscription in which the Log Analytics workspace exists.  This may be ommitted if the subscription is already in
+the current context.
+
+.PARAMETER ExpirationThresholdDays
+Specify a threshold in days to which the certificate expiration date will be compared.  If the expiration date falls within this threshold, 
+then the data about those certificates will be forwarded to Log Analytics.  A 'falsey' value will forward all certificate metadata.
+
+.PARAMETER ValidTags
+Provide a list of tags that should be forwarded to Log Analytics.  If a specified valid tag is found on the certificate, the value of that tag
+is sent to Log Analytics.  Tags that aren't specified as Valid Tags will not be forwarded.
+
+.EXAMPLE
+This exmaple shows how to pull data about certificates from the 'django-keyvault' to the 'webapp-workspace.'  Because of the threshold set to 90 days, only certificates
+expiring within the next 90 days will be forwarded to Log Analytics.
+
+Send-KVCertInfo -VaultResourceGroup django -VaultName django-keyvault -LogAnalyticsWorkspaceName webapp-workspace -Verbose -LogAnalyticsResourceGroup django-webapps -ExpirationThresholdDays 90
+#>
 function Send-KVCertInfo
 {
     [CmdletBinding()]
@@ -41,6 +81,23 @@ function Send-KVCertInfo
         )
     )
 
+    $currentSub = (Get-AzureRmContext).Subscription.Name
+
+    if (-not $VaultSubscriptionName)
+    {
+        $VaultSubscriptionName = $currentSub
+    }
+
+    if (-not $LogAnalyticsSubscriptionName)
+    {
+        $LogAnalyticsSubscriptionName = $currentSub
+    }
+
+    if ($currentSub -ne $VaultSubscriptionName)
+    {
+        Set-Context -SubscriptionName $VaultSubscriptionName -Reason "to access KeyVault in $VaultSubscriptionName"
+    }
+
     $certificates = Get-AzureKeyVaultCertificate -VaultName $VaultName
     $certCollection = @()
 
@@ -58,6 +115,7 @@ function Send-KVCertInfo
     $WorkspaceKey = Get-WorkspaceKey -WorkspaceName $LogAnalyticsWorkspaceName -ResourceGroupName $LogAnalyticsResourceGroup -SubscriptionName $LogAnalyticsSubscriptionName
     Write-Verbose "Found the workspace key"
 
+    Set-Context -SubscriptionName $VaultSubscriptionName -Reason "set to Vault Sub to get certificate data"
     $ExtractDate = Get-Date -Format "yyyyMMdd hh:mm:ss"
 
     foreach ($certificate in $certificates)
@@ -117,5 +175,3 @@ function Send-KVCertInfo
 
     $certCollection
 }
-
-# Send-KVCertInfo -VaultResourceGroup django -VaultName django-keyvault -LogAnalyticsWorkspaceName ox-workspace -Verbose -LogAnalyticsResourceGroup gov -ExpirationThresholdDays 90
